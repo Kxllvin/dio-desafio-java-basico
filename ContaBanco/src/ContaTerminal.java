f7f12c644e4cfedcdcc4dd33b65491242feb5095
Kxllvin <kelvintenor987@gmail.com>
2024-04-27 12:31:20 -0300

    código incrementado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,28 @@
-public class ContaTerminal {
+import java.util.Scanner;
+
+public class  ContaTerminal {
+
+    static int num = 1021;
+    static String agencia = "067";
+    static String digt = "8";
+    static String nome = "MARIO ANDRADE";
+    static double saldo = 237.48;
+
         public static void main(String[] args) throws Exception {
-            System.out.println("Hello, World!");
+    
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Por favor, digite o número da Agência: ");
+            String agn = scanner.next();
+            System.out.println("Por favor, digite o número da Conta: ");
+            int cnt = scanner.nextInt();
+
+            
+            if ( agn.equals(agencia) && cnt == num){
+                System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + "-" + digt + " Conta " + num + ", com saldo de R$" + saldo + " disponível para saque.");
+                        } else {
+                            System.out.println("Não há cliente registrado.");
+                        };
+            scanner.close();
         }
     
 }
